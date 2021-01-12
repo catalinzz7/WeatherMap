25e7091d1962f7b52439e556d41daa3ec9fdd0a5
Catalin Raceanu <catalin.raceanu@mta.ro>
2021-01-12 18:54:10 +0200

    Update Main
    Am adaugat functionalitati clasei main
    pentru a rula programul si a afisa
    fereastra principala
    
    Signed-off-by: Catalin Raceanu <catalin.raceanu@mta.ro>

--- src/main/java/ro/mta/se/lab/Main.java
+++ src/main/java/ro/mta/se/lab/Main.java
@@ -16,11 +16,30 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-
+        this.primaryStage = primaryStage; // connect primary stage
+        mainWindow();
     }
 
+    // main window
+    public void mainWindow() {
+        try {
+            // view
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("/view/WeatherView.fxml"));
+            AnchorPane pane = loader.load();
 
+            // controller
+            WeatherManager mainWindowController = loader.getController();
+            mainWindowController.setMain(this);
+
+            // scene on stage
+            Scene scene = new Scene(pane);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
