20824ab400bb1c2c7283480271566daea40ab3ed
Catalin Raceanu <catalin.raceanu@mta.ro>
2021-01-12 19:16:29 +0200

    Adaugare clasa Location
    Am adaugat clasa Location
    pentru a salva datele citite din
    fisierul de intrare despre localitati
    
    Signed-off-by: Catalin Raceanu <catalin.raceanu@mta.ro>

--- src/main/java/ro/mta/se/lab/model/Location.java
+++ src/main/java/ro/mta/se/lab/model/Location.java
@@ -1,2 +1,39 @@
-package ro.mta.se.lab.model;public class Location {
+package ro.mta.se.lab.model;
+
+public class Location {
+
+    String Country;
+    String City;
+
+    Double Lon;
+    Double Lat;
+
+    public Location(String country, String city, Double lon, Double lat)
+    {
+        this.Country = country;
+        this.City = city;
+        this.Lon = lon;
+        this.Lat = lat;
+    }
+
+    public String getCountry()
+    {
+        return Country;
+    }
+
+    public String getCity()
+    {
+        return City;
+    }
+
+    public Double getLon()
+    {
+        return Lon;
+    }
+
+    public Double getLat()
+    {
+        return Lat;
+    }
+
 }
